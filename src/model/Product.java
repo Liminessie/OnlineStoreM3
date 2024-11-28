915c888bbfe483ca55fb0701fa1b2cc1da3c4065
C3rb3rux-code <polcrepal@campus.monlau.com>
2024-11-28 17:52:17 +0100

    Constructor ready

--- src/model/Product.java
+++ src/model/Product.java
@@ -10,4 +10,18 @@ package model;
  */
 public class Product {
     private String name;
+    private String description;
+    private float supplierPrice;
+    private float salePrice;
+    private int category;
+    private String color;
+
+    public Product(String productName, String productDescription, float productSupplierPrice, float productSalePrice, int productCategory, String productColor) {
+        this.name = productName;
+        this.description = productDescription;
+        this.supplierPrice = productSupplierPrice;
+        this.salePrice = productSalePrice;
+        this.category = productCategory;
+        this.color = productColor;
+    }
 }
