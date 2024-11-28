88e25ad4474181562787642b23cb3c76866bca7b
C3rb3rux-code <polcrepal@campus.monlau.com>
2024-11-28 17:58:46 +0100

    Getter Setter & Constructor

--- src/model/Product.java
+++ src/model/Product.java
@@ -24,4 +24,66 @@ public class Product {
         this.category = productCategory;
         this.color = productColor;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getSupplierPrice() {
+        return supplierPrice;
+    }
+
+    public void setSupplierPrice(float supplierPrice) {
+        this.supplierPrice = supplierPrice;
+    }
+
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Product{");
+        sb.append("name= ").append(name);
+        sb.append(", description= ").append(description);
+        sb.append(", supplierPrice= ").append(supplierPrice);
+        sb.append(", salePrice= ").append(salePrice);
+        sb.append(", category= ").append(category);
+        sb.append(", color= ").append(color);
+        sb.append('}');
+        return sb.toString();
+    }
 }
