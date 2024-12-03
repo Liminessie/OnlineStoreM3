68b62cc2962c8c8b6458bfef15d91e62075f3306
C3rb3rux-Code <polcrepal@campus.monlau.com>
2024-12-03 16:04:40 +0100

    Try

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author cesararcras
- */
 public class Product {
+    
     private String name;
     private String description;
     private float supplierPrice;
