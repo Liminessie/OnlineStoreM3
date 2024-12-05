56a452fe4a00da01b445a231d64d1bb395a47b97
C3rb3rux-Code <polcrepal@campus.monlau.com>
2024-12-05 16:18:38 +0100

    ProductController

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,5 +1,30 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.Product;
+
 public class ProductController {
-    private String name;
+    
+    ArrayList<Product> productList = new ArrayList<>();
+
+    public void addProduct(String name, String description, float supplierPrice, float salePrice, int category, String color) {
+        productList.add(new Product(name, description, supplierPrice, salePrice, category, color));
+    }
+
+    public void findProduct() {
+        productList.size();
+    }
+
+    public void updateProduct() {
+        
+    }
+
+    public void deleteProduct() {
+
+    }
+
+    public void listProduct() {
+        
+    }
 }
